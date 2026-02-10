7a07aeb66c4e8831acf52452b737a75409370f1d
Shreyas <shreyasajaymenon@gmail.com>
2026-02-10 20:13:02 +0530

    addition.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#Calculator 
+#Calculator
+
+import addition as a
+import sub as s
+import nul as n
+
+print("Addition = ", a.add(5,6))
+print("Subtraction = ", s.sub(5,6))
+print("Multiplication = ", n.nul(5,6))
